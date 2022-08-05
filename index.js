393567820e59c86b5dbcb7bacc6bd3c689c8853f
Ashley Suhr <ashy.suhr@gmail.com>
2022-08-06 01:00:15 +0200

    initialize github repository

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const Discord = require('discord.js')
+require('dotenv').config()
 
+const client = new Discord.Client({intents: [
+  "Guilds",
+  "GuildMessages",
+  "GuildMembers"
+]})
 
-const commands = [];
-const commandsPath = path.join(__dirname, 'commands' );
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	commands.push(command.data.toJ ,SON());
-}
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`)
+})
 
-const rest = new REST({ version: '9' }).setToken(token);
-
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
+client.on("messageCreate", (message) =>{
+  if (message.content == "Hi"){
+    message.reply("Hello world")
   }
-})();
+})
